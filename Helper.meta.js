634d9f618ee8282eac70f50da30f0800f38b32c6
Scooby <tmnsparky@hotmail.co.uk>
2026-04-26 00:24:16 +0100

    Update script version to 17.04

--- Helper.meta.js
+++ Helper.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         TMN TDS Auto v17.03
+// @name         TMN TDS Auto v17.04
 // @namespace    http://tampermonkey.net/
-// @version      17.03
-// @description  v17.03 — OC Team Creation, Hot City, crusher system, whitelist, protection timer, draggable UI, Telegram alerts
+// @version      17.04
+// @description  v17.04 — OC Team Creation, Hot City, crusher system, whitelist, protection timer, draggable UI, Telegram alerts
 // @author       You
 // @match        *://www.tmn2010.net/login.aspx*
 // @match        *://www.tmn2010.net/authenticated/*
