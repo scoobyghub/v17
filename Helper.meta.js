17ecfb04ed862be05502ff1da03f205ad72d18ca
Scooby <tmnsparky@hotmail.co.uk>
2026-04-25 13:22:53 +0100

    Add metadata for TMN TDS Auto user script

---
+++ Helper.meta.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         TMN TDS Auto v17.02
+// @namespace    http://tampermonkey.net/
+// @version      17.02
+// @description  v17.02 — OC Team Creation, Hot City, crusher system, whitelist, protection timer, draggable UI, Telegram alerts
+// @author       You
+// @match        *://www.tmn2010.net/login.aspx*
+// @match        *://www.tmn2010.net/authenticated/*
+// @match        *://www.tmn2010.net/Login.aspx*
+// @match        *://www.tmn2010.net/Authenticated/*
+// @match        *://www.tmn2010.net/Default.aspx*
+// @match        *://www.tmn2010.net/default.aspx*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @connect      api.telegram.org
+// @updateURL    https://raw.githubusercontent.com/scoobyghub/v17/refs/heads/main/Helper.meta.js
+// @downloadURL  https://raw.githubusercontent.com/scoobyghub/v17/refs/heads/main/Helper.user.js
+// ==/UserScript==
